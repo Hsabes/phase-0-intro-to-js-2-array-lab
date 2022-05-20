fd0334523a3b4016ba48e375310ddf3e458e7189
Harrison Sabean <hsabes@gmail.com>
2022-05-19 22:23:59 -0700

    commit tree 9190197aced4247e7e472a005135c032ab5b78d6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(){
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(){
+    const newCats1 = [...cats, 'Broom'];
+    return newCats1;
+}
+
+function prependCat(){
+    const newCats2 = ['Arnold', ...cats,];
+    return newCats2;
+}
+
+function removeLastCat(){
+    return cats.slice(0,2);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
